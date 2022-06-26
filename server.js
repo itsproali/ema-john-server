62188f21166209d695dc7763bd5dfb4fe74192bf
Mohammad Ali <itsproali@gmail.com>
2022-06-27 00:47:31 +0600

    JWT Removed

--- server.js
+++ server.js
@@ -10,22 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  console.log(authHeader);
-  if (!authHeader) {
-    res.status(401).send({ message: "Reauthorized Access" });
-  }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-    if (err) {
-      res.status(403).send({ message: "Forbidden Access" });
-    }
-    req.decoded = decoded;
-    next();
-  });
-};
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster1.jggf1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -63,28 +47,16 @@ async function run() {
     });
 
     // Setting Cart Product
-    app.post("/cartProducts", verifyJWT, async (req, res) => {
+    app.post("/cartProducts", async (req, res) => {
       const userId = req.body.userId;
-      const decodedUserId = req.decoded.userId;
-
-      if (userId === decodedUserId) {
         const keys = req.body.keys;
         const ids = keys.map((id) => ObjectId(id));
         const query = { _id: { $in: ids } };
         const cursor = productCollection.find(query);
         const result = await cursor.toArray();
         res.send(result);
-      }
     });
 
-    // Get Token
-    app.post("/getToken", async (req, res) => {
-      const userId = req.body.userId;
-      const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN, {
-        expiresIn: "1d",
-      });
-      res.send({ accessToken });
-    });
   } finally {
     // client.close()
   }
